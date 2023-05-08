b546f37acfb752636ad7592bc782448d8cc3780e
UJIN <121651511+ugincoo@users.noreply.github.com>
2023-05-08 12:21:31 +0900

    [unrated] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -0,0 +1,8 @@
+-- 코드를 입력하세요
+SELECT TITLE ,board.BOARD_ID ,REPLY_ID , reply.WRITER_ID ,reply.CONTENTS 
+, DATE_FORMAT(reply.CREATED_DATE,"%Y-%m-%d")as CREATED_DATE 
+from USED_GOODS_REPLY as reply
+left join USED_GOODS_BOARD as board
+on reply.BOARD_ID = board.BOARD_ID  
+where  DATE_FORMAT(board.CREATED_DATE,'%Y-%m' ) = '2022-10'
+order by  CREATED_DATE ,TITLE
